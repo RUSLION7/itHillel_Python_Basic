5ff76e0ac1e54350ef60cf0059ed1c8f6202461b
RUSLION7 <rus.zuyev@gmail.com>
2025-11-13 13:18:52 +0200

    HW_7.2

---
+++ homeworks/HW_7.2.py
@@ -0,0 +1,20 @@
+def correct_sentence(text):
+    # Робимо великою тільки першу літеру, не чіпаючи решту рядка.
+    corrected_text = text[0].upper() + text[1:]
+
+    # Додаємо крапку в кінці, якщо її немає.
+    if not corrected_text.endswith('.'):
+        corrected_text += '.'
+
+    # Повертаємо виправлений рядок.
+    return corrected_text
+
+
+# Перевірочні тести.
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+
+print('ОК')
